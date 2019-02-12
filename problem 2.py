5431e774b551993dabc9c368757aed4daaf46a1e
xsouffront <43756495+xsouffront@users.noreply.github.com>
2019-02-12 12:05:05 -0500

    Add files via upload

---
+++ problem 2.py
@@ -0,0 +1,8 @@
+from turtle import *
+abc= Turtle()
+abc.color('Red')
+abc.pensize(2)
+for x in range(4):
+	abc.forward(100)
+	abc.left(90)
+mainloop()
---
+++ problem 3.py
@@ -0,0 +1,8 @@
+from turtle import *
+abc= Turtle()
+abc.color('Red')
+abc.pensize(2)
+for x in range(20):
+	abc.forward(100)
+	abc.left(30)
+mainloop()
