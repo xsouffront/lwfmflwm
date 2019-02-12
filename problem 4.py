b61045260ec8bac877da1cc3907c7281e3961e50
xsouffront <43756495+xsouffront@users.noreply.github.com>
2019-02-12 12:06:07 -0500

    Add files via upload

---
+++ problem 4.py
@@ -0,0 +1,11 @@
+from turtle import *
+abc= Turtle()
+abc.color('Red')
+abc.pensize(2)
+for x in range(3):
+	abc.forward(100)
+	abc.left(120)
+cba= Turtle()
+cba.color('Pink')
+cba.circle(25)
+mainloop()
